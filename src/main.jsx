01788117884c4c180188e1efb314814f6c883354
yayamehdi715-dot <yayamehdi715@email.com>
2026-03-11 16:51:34 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { initPixel } from './utils/pixel'
-
-// Initialise Meta Pixel au démarrage de l'app
-initPixel()
+// Le Pixel Meta est initialisé directement dans index.html (fbq init + PageView base)
+// Les événements avancés sont gérés via src/utils/metaPixel.js
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
